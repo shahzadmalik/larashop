db4bacba8d9abac19633b190ca0762c6cf74b383
shahzad <shahzadmajoka@gmail.com>
2018-11-14 10:24:29 +0000

    database setup with migrations

---
+++ app/Steps.php
@@ -0,0 +1,13 @@
+<?php 
+/*steps to make ecommerce site in laravel 5.6 and above
+1. Remove Name field from user Table.
+2. Create following migrations and Models
+1. php artisan make:model Profile -mcr 
+2. php artisan make:model Product -mcr 
+3. php artisan make:model Order -mcr 
+4. php artisan make:model Payment -mcr
+5. php artisan make:model Role -mcr
+6. php artisan make:model Category -mcr 
+SET YOUR PACKAGE.JSON FILE ACCORDING TO YOUR NEED
+7. php artisan preset none // will remove unnecessary dependenceis
+8.php artisan present bootstrap
